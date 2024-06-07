a4f180566711f73f83608d3bfcd7ea7239efdde8
Vicente-VP <108531836+Vicente-VP@users.noreply.github.com>
2024-06-07 18:57:57 +0000

    V1 - Versão Inicial (Vicente e Vinicius)

---
+++ models/teams.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const team = new mongoose.Schema({
+  nome_team: {
+    type: String,
+    required: true,
+  },
+  coach: {
+    type: String,
+    required: true,
+  },
+  president: {
+    type: String,
+    required: true,
+  },
+  stadium: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  team_shield: {
+    type: String,
+    required: true,
+  },
+});
+
+const Teams = mongoose.model('Teams', teamSchema);
+
+module.exports = Teams;
---
+++ routes/team_routes.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+const Teams = require('../models/teams');
+
+// Rota para obter todos os team
+router.get('/', async (req, res) => {
+  try {
+    const team = await Teams.find();
+    res.json(team);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Rota para obter um teams por ID
+router.get('/:id', getTeams, (req, res) => {
+  res.json(res.teams);
+});
+
+// Rota para criar um novo teams
+router.post('/', async (req, res) => {
+  const teams = new Teams({
+    nome_team: req.body.nome_team,
+    coach: req.body.coach,
+    president: req.body.president,
+    stadium: req.body.stadium,
+    city: req.body.city,
+    team_shield: req.body.team_shield,
+  });
+
+  try {
+    const newTeams = await teams.save();
+    res.status(201).json(newTeams);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Rota para atualizar um teams por ID
+router.put('/:id', getTeams, async (req, res) => {
+  if (req.body.nome_team != null) {
+    res.teams.nome_team = req.body.nome_team;
+  }
+  if (req.body.coach != null) {
+    res.teams.coach = req.body.coach;
+  }
+  if (req.body.president != null) {
+    res.teams.president = req.body.president;
+  }
+  if (req.body.stadium != null) {
+    res.teams.stadium = req.body.stadium;
+  }
+  if (req.body.city != null) {
+    res.teams.city = req.body.city;
+  }
+  if (req.body.team_shield != null) {
+    res.teams.team_shield = req.body.team_shield;
+  }
+
+  try {
+    const updatedTeams = await res.teams.save();
+    res.json(updatedTeams);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Rota para excluir um teams por ID
+router.delete('/:id', getTeams async (req, res) => {
+  try {
+    await res.teams.deleteOne();
+    res.json({ message: 'Time excluído com sucesso!' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getTeams(req, res, next) {
+  try {
+    const teams = await Teams.findById(req.params.id);
+    if (teams == null) {
+      return res.status(404).json({ message: 'Time não encontrado' });
+    }
+    res.teams = teams;
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+}
+
+module.exports = router;
