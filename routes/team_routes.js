b5faf3f542fc2e384477d281b7970534e205c805
Vicente Pascoal <108531836+Vicente-VP@users.noreply.github.com>
2024-09-27 02:00:01 +0000

    Adicionando ID

--- routes/team_routes.js
+++ routes/team_routes.js
@@ -77,7 +77,7 @@ router.put('/:id', getTeams, async (req, res, next) => {
 
  
 // Rota para excluir um teams por ID
-router.delete('/', getTeams, async (req, res, next) => {
+router.delete('/:id', getTeams, async (req, res, next) => {
   try {
     await res.teams.deleteOne();
     res.json({ message: 'Time excluído com sucesso!' });
